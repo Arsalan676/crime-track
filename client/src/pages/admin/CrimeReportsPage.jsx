d7251825196f994c1bd166d97a8e7d07dbd3a388
Arsalan Aziz <arsalanaziz85@gmail.com>
2025-12-26 11:16:22 +0530

    Change file name

--- client/src/pages/admin/CrimeReportsPage.jsx
+++ client/src/pages/admin/CrimeReportsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import adminService from "../../services/adminService";
 import authService from "../../services/authService";
-import AdminCrimeMap from "../../components/Map/AdminCrimeMap";
+import AdminCrimeMap from "../../components/map/AdminCrimeMap";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
--- client/src/pages/user/ReportCrime.jsx
+++ client/src/pages/user/ReportCrime.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import authService from "../../services/authService.js";
 import reportService from "../../services/reportService.js";
-import UserLocationMap from "../../components/map/UserLocationMap";
+import UserLocationMap from "../../components/map/UserLocationMap.jsx";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
